7daf37d78fc7667152197100d237a5a389e415d8
Jeff McAffer <jeffmcaffer@github.com>
2021-03-08 09:13:18 -0800

    add mp4 renderer

---
+++ render/application/mp4/file.js
@@ -0,0 +1,16 @@
+
+(content, options, context) => {
+  const rawURL = `${content}?raw=true`
+  const html = `
+    <div className="egh-center-justified cssClass" >
+      <div className="egh-tm-10" >
+        <video id="rendered" className="video-js vjs-big-play-centered" >
+          <source src=${rawURL} type="video/mp4" />
+        </video>
+      </div >
+    </div >`
+  const css = { url: 'https://vjs.zencdn.net/7.10.2/video-js.css' }
+  const script = {url: 'https://vjs.zencdn.net/7.10.2/video.min.js'}
+
+  return { html, css, script }
+}
