4b68c88a2b1c9eb3f7805960015c9f2f695ec4fe
Jeff McAffer <jeffmcaffer@github.com>
2021-03-30 10:32:50 -0700

    return compelete html doc

--- render/video/mp4/file.js
+++ render/video/mp4/file.js
@@ -3,15 +3,26 @@
   const { _ } = context
   const { controls, autoplay } = inputs
   const setup = { controls: _.defaultTo(controls, true), autoplay: _.defaultTo(autoplay, false) }
-  const rawURL = `${content}?raw=true`
+  const title = content.split('/').slice(-1)[0]
   const html = `
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${title}</title>
+    <link rel="stylesheet" href="https://vjs.zencdn.net/7.10.2/video-js.css">
+    <script type="application/javascript" src="https://vjs.zencdn.net/7.10.2/video.min.js" />
+  </head>
+  <body>
     <div class="egh-center-justified" >
       <div class="egh-tm-10" >
         <video id="rendered" class="video-js vjs-big-play-centered" preload="auto" data-setup='${JSON.stringify(setup)}' >
-          <source src=${rawURL} type="video/mp4" />
+          <source src=${content} type="video/mp4" />
         </video>
       </div >
-    </div >`
+    </div >
+  </body>
+</html>`
   const styles = { url: 'https://vjs.zencdn.net/7.10.2/video-js.css' }
   const script = { url: 'https://vjs.zencdn.net/7.10.2/video.min.js' }
 
