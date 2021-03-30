34e0c40c7070191924b90ad0ec5541687652cf87
Jeff McAffer <jeffmcaffer@github.com>
2021-03-30 16:24:36 -0700

    update parent DOM comms to use postMessage

--- render/video/mp4/file.js
+++ render/video/mp4/file.js
@@ -3,7 +3,8 @@
   const { _ } = context
   const { controls, autoplay } = inputs
   const setup = { controls: _.defaultTo(controls, true), autoplay: _.defaultTo(autoplay, false) }
-  const title = content.split('/').slice(-1)[0]
+  const fileAndQuery = content.split('/').slice(-1)[0]
+  const title = fileAndQuery.split('?').slice(0, 1)[0]
   const html = `
 <html>
   <head>
@@ -11,17 +12,26 @@
     <link rel="stylesheet" href="https://vjs.zencdn.net/7.10.2/video-js.css"></link>
     <script type="application/javascript" src="https://vjs.zencdn.net/7.10.2/video.min.js"></script>
     <script>
-      $(document).ready(function () {
+      const register = function () {
         videojs("rendered", {}, function() {
-          this.on('loadedalldata', () => window.parent.dispatchEvent('DOMContentLoaded'))
+          // setup a listener to tell the parent DOM that the child is "ready"
+          this.on('ready', function () { 
+            window.parent.postMessage('renderedReady', 'https://github.com')
+          })
         })
-      })
+      }
+      // hook into parent lifecycle to register our child listener
+      if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(register, 1)
+      } else {
+        document.addEventListener("DOMContentLoaded", register, false)
+      }
     </script>
   </head>
   <body>
     <div class="egh-center-justified" >
       <div class="egh-tm-10" >
-        <video id="rendered" class="video-js vjs-big-play-centered" preload="auto" data-setup='${JSON.stringify(setup)}' >
+        <video id="rendered" class="video-js vjs-big-play-centered vjs-fill" preload="auto" data-setup='${JSON.stringify(setup)}' >
           <source src=${content} type="video/mp4" />
         </video>
       </div >
@@ -33,3 +43,10 @@
 
   return { html, styles, script }
 }
+
+
+// $(document).ready(function () {
+//   videojs("rendered", {}, function() {
+//     this.on('loadedalldata', () => window.parent.dispatchEvent('DOMContentLoaded'))
+//   })
+// })
