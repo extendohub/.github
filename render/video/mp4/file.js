2159e4be2cd5c885c6ded8f7825aa91c192126c1
Jeff McAffer <jeffmcaffer@github.com>
2021-03-30 19:05:39 -0700

    revert to returning doc fragments

--- render/video/mp4/file.js
+++ render/video/mp4/file.js
@@ -1,44 +1,17 @@
-({ content, inputs, context }) => {
+({ content, options, context }) => {
   const { _ } = context
   const { controls, autoplay } = inputs
-  const setup = { controls: _.defaultTo(controls, true), autoplay: _.defaultTo(autoplay, false), preload: 'auto' }
-  const fileAndQuery = content.split('/').slice(-1)[0]
-  const title = fileAndQuery.split('?').slice(0, 1)[0]
+  const setup = { controls: _.defaultTo(controls, true), autoplay: _.defaultTo(autoplay, false) }
+  const rawURL = `${content}?raw=true`
   const html = `
-  <html>
-  <head>
-  <title>${title}</title>
-  <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/video.js/7.11.7/video-js.min.css" integrity="sha512-fteJK9ZlHSFXwfkV3ASsRd6CLzPIK1C5wCpbDfYJaRmjUiezOU28xJwrALF+agINF8c6do/EOXXOqI2IDPUwQw==" crossorigin="anonymous" />
-  </head>
-  <body>
-    <div class="egh-center-justified" >
-      <div class="egh-tm-10" >
-        <video id="rendered" class="video-js vjs-big-play-centered vjs-fill" data-setup='${JSON.stringify(setup)}' >
-          <source src="${content}" type="video/mp4" />
-        </video>
-      </div >
-    </div >
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/video.js/7.11.7/video.min.js" integrity="sha512-+n1LqBtKBxGt2673VCcanmzSLgQ8Mfi9NCjg6wI9UOSH//GIBD+2Oe+hFqLdpMMXkuNuGOuPPv+qI3tfq6p9Hw==" crossorigin="anonymous"></script>
-    <script>
-      const register = function () {
-          videojs("rendered", {}, function() {
-            // setup a listener to tell the parent DOM that the child is "ready"
-            this.on('ready', function () { 
-              window.parent.postMessage('renderedReady', 'https://github.com')
-          })
-        })
-      }
-      // hook into parent lifecycle to register our child listener
-      if (document.readyState === "complete" || document.readyState === "interactive") {
-        setTimeout(register, 1)
-      } else {
-        document.addEventListener("DOMContentLoaded", register, false)
-      }
-    </script>
-  </body>
-</html>
-`
-  return { html }
+      <div class="egh-center-justified" >
+        <div class="egh-tm-10" >
+          <video id="rendered" class="video-js vjs-big-play-centered" preload="auto" data-setup='${JSON.stringify(setup)}' >
+            <source src=${rawURL} type="video/mp4" />
+          </video>
+        </div >
+      </div >`
+  const styles = { url: 'https://vjs.zencdn.net/7.10.2/video-js.css' }
+  const script = { url: 'https://vjs.zencdn.net/7.10.2/video.min.js' }
+  return { html, styles, script }
 }
