fd2a41293f1b7b487163d140309ec66ec6dd6210
Jeff McAffer <jeffmcaffer@github.com>
2021-04-03 18:08:13 -0700

    remove debugging and dead code.

--- render/application/vnd.openxmlformats-officedocument.spreadsheetml.sheet/index.js
+++ render/application/vnd.openxmlformats-officedocument.spreadsheetml.sheet/index.js
@@ -1,8 +1,6 @@
 () => {
   const script = `
   async function _load() {
-    // const url = 'https://github.com/app-extensions/test/blob/main/test.xlsx?raw=true'
-    // const url ='file:///C:/git/extensions/extendo-render/xlsx/test.xlsx'
     const url = location.href + '?raw=true'
     const response = await fetch(url, { mode:"cors"})
     if (!response.ok) return
@@ -14,7 +12,6 @@
   }
 
   const run = async () => {
-    debugger
     const json = await _load()
     const mountPoint = document.getElementById('xlsx-render')
     const cdg = canvasDatagrid({
@@ -23,19 +20,9 @@
     cdg.style.height = '100%'
     cdg.style.width = '100%'
 
-    const _resize = () => {
-      // mountPoint.style.height = (window.innerHeight - 200) + "px"
-      // mountPoint.style.width = (window.innerWidth - 200) + "px"
-      mountPoint.style.height = (window.innerHeight - 200) + "px"
-      mountPoint.style.width = (mountPoint.parent.scrollWidth) + "px"
-    }
-    
-    // window.addEventListener('resize', _resize)
-
     mountPoint.style.display = "block"
-    // _resize()
 
-    /* set up table headers */
+    // pad headers
     var L = 0
     json.forEach(function (r) { if (L < r.length) L = r.length })
     for (var i = json[0].length; i < L; ++i) {
