d07a8ce8772a902ba642c5d9287f72df6e9b5a42
Jeff McAffer <jeffmcaffer@github.com>
2021-04-19 10:29:49 -0700

    fix csv function

--- render/data/htmlTable.js
+++ render/data/htmlTable.js
@@ -9,21 +9,21 @@ async ({ content, context, options }) => {
     lines.push(` <tr><td>${row.join(`</td><td>`)}</td></tr>`))
   lines.push('</table>')
   return { html: lines.join('\n') }
-}
 
-function parseCSV(data, delimiter) {
-  delimiter = delimiter || ","
-  const pattern = new RegExp(
-    "(\\" + delimiter + "|\\r?\\n|\\r|^)" + "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" + "([^\"\\" + delimiter + "\\r\\n]*))", "gi"
-  )
-  const result = [[]]
-  let matches = null
-  while (matches = pattern.exec(data)) {
-    var matchedDelimiter = matches[1]
-    if (matchedDelimiter.length && matchedDelimiter !== delimiter)
-      result.push([])
-    const matchedValue = matches[2] ? matches[2].replace(new RegExp("\"\"", "g"), "\"") : matches[3]
-    result[result.length - 1].push(matchedValue)
+  function parseCSV(data, delimiter) {
+    delimiter = delimiter || ","
+    const pattern = new RegExp(
+      "(\\" + delimiter + "|\\r?\\n|\\r|^)" + "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" + "([^\"\\" + delimiter + "\\r\\n]*))", "gi"
+    )
+    const result = [[]]
+    let matches = null
+    while (matches = pattern.exec(data)) {
+      var matchedDelimiter = matches[1]
+      if (matchedDelimiter.length && matchedDelimiter !== delimiter)
+        result.push([])
+      const matchedValue = matches[2] ? matches[2].replace(new RegExp("\"\"", "g"), "\"") : matches[3]
+      result[result.length - 1].push(matchedValue)
+    }
+    return result
   }
-  return result
 }
