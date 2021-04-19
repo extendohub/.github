0e80a19aceddc33539a140bc41fbda8f597cca83
Jeff McAffer <jeffmcaffer@github.com>
2021-04-18 22:44:42 -0700

    promote vega renderer

--- render/data/vega.js
+++ render/data/vega.js
@@ -1,5 +1,6 @@
-async ({ content, options }) => {
+async ({ content, options, context }) => {
   const data = typeof (content) === 'string' ? { url: content } : { values: content }
+  const unique = context.render.hash.slice(-5)
   const script = `
     const vlSpec = {
       $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
@@ -8,14 +9,23 @@ async ({ content, options }) => {
       mark: '${options.mark}',
       encoding: ${JSON.stringify(options.encoding, null, 2)}
     };
-    debugger
-    vegaEmbed('#vis', vlSpec);
+    vegaEmbed('#vis${unique}', vlSpec);
   `
-  const scripts = [
-    { url: 'https://cdn.jsdelivr.net/npm/vega@5.20.2' },
-    { url: 'https://cdn.jsdelivr.net/npm/vega-lite@5.1.0' },
-    { url: 'https://cdn.jsdelivr.net/npm/vega-embed@6.17.0' }
-  ]
-  html = `<div id="vis"></div>`
-  return { html, scripts, bodyScripts: [script] }
+  const html = `
+<html>
+  <head>
+    <title>Embedding Vega-Lite</title>
+    <script src="https://cdn.jsdelivr.net/npm/vega@5.20.2"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vega-lite@5.1.0"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vega-embed@6.17.0"></script>
+  </head>
+  <body>
+    <div id="vis${unique}"></div>
+    <script type="text/javascript">
+      ${script}
+    </script>
+  </body>
+</html>
+`
+  return { html }
 }
