fe81dac7ce017227125d8d69c0ef895fbae911ec
Jeff McAffer <jeffmcaffer@github.com>
2021-05-21 13:26:48 -0700

    place import outside function

--- render/github/ghnb.js
+++ render/github/ghnb.js
@@ -12,14 +12,13 @@ async ({ options, context }) => {
   const stringModule = `data:text/javascript;base64,${encoded}`
   const script = `
     <script type="module">
-      // (async () => {
-        console.log('in the code')
-        import * as obserables from 'https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js'
-        const { Runtime, Inspector } = observables
-        // const { define } = await import(${stringModule})
-        // const runtime = new Runtime()
-        // const main = runtime.module(define, Inspector.into(document.body))
-      // })()
+      console.log('in the code')
+      import { Runtime, Inspector } from 'https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js'
+      (async () => {
+        const { define } = await import(${stringModule})
+        const runtime = new Runtime()
+        const main = runtime.module(define, Inspector.into(document.body))
+      })()
     </script>`
   const html = `<html>
   <meta charset="utf-8">
