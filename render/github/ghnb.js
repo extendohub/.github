791c335892fcb3367752417470c09f5e594732db
Jeff McAffer <jeffmcaffer@github.com>
2021-05-21 13:11:52 -0700

    add some debugging

--- render/github/ghnb.js
+++ render/github/ghnb.js
@@ -11,19 +11,20 @@ async ({ options, context }) => {
   const encoded = new Buffer(markdown).toString('base64');
   const stringModule = `data:text/javascript;base64,${encoded}`
   const script = `
-      <script type="module">
-        (async () => {
-          import { Runtime, Inspector } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js"
-          const { define } = await import(${stringModule})
-          const runtime = new Runtime()
-          const main = runtime.module(define, Inspector.into(document.body))
-        })()
-      </script>`
+    <script type="module">
+      (async () => {
+        console.log('in the code')
+        import { Runtime, Inspector } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js"
+        const { define } = await import(${stringModule})
+        const runtime = new Runtime()
+        const main = runtime.module(define, Inspector.into(document.body))
+      })()
+    </script>`
   const html = `<html>
   <meta charset="utf-8">
   <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@observablehq/inspector@3/dist/inspector.css">
   <body>
-    ${script}
+    ${script.trim()}
   </body>
 </html>
 `
