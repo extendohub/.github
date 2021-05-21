b53c04a6046e46ca87af604f32e9518327a0d3eb
Jeff McAffer <jeffmcaffer@github.com>
2021-05-21 12:30:03 -0700

    add ghnb rendering based on observable runtime

---
+++ render/github/ghnb.js
@@ -0,0 +1,30 @@
+async ({ options, context }) => {
+  function splitMarkdown(content) {
+    const matches = content.match(/^---\n(.*)---/sm)
+    if (matches && matches.length === 2)
+      return { frontmatter: matches[1], markdown: content.slice(matches[0].length + matches.index) }
+    return { frontmatter: undefined, markdown: content }
+  }
+  
+  const content = await context.render.getContent({})
+  const { frontmatter, markdown } = splitMarkdown(content)
+  const encoded = `data:text/javascript;base64,${btoa(markdown)}`
+  const script = `
+      <script type="module">
+        (async () => {
+          import { Runtime, Inspector } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js"
+          const { define } = await import(${encoded})
+          const runtime = new Runtime()
+          const main = runtime.module(define, Inspector.into(document.body))
+        })()
+      </script>`
+  const html = `<html>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@observablehq/inspector@3/dist/inspector.css">
+  <body>
+    ${script}
+  </body>
+</html>
+`
+  return { html }
+}
