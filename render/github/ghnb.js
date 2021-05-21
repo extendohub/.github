5b9c1acd05af3442183389988ae96876d5b7f506
Jeff McAffer <jeffmcaffer@github.com>
2021-05-21 13:19:04 -0700

    trim 2

--- render/github/ghnb.js
+++ render/github/ghnb.js
@@ -14,7 +14,7 @@ async ({ options, context }) => {
     <script type="module">
       (async () => {
         console.log('in the code')
-        import { Runtime, Inspector } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js"
+        // import { Runtime, Inspector } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js"
         // const { define } = await import(${stringModule})
         // const runtime = new Runtime()
         // const main = runtime.module(define, Inspector.into(document.body))
