3d7678dcb5de24d5977393dbb68e5d6414ab2588
Jeff McAffer <jeffmcaffer@github.com>
2021-06-23 14:46:48 -0700

    update figma url handling

--- render/application/figma.js
+++ render/application/figma.js
@@ -1,13 +1,21 @@
 const https = require('https')
 
 module.exports = async ({ content, context }) => {
-  const [file, node] = content.trim().split('#')
+  let file = null
+  let node = null
+  if (content.startsWith('https://figma.com/file')) {
+    const matches = content.match(/file\/(.*?)\/.*?node-id=(.*)(?:(?:%3A)|:)(.*)$/)
+    if (!matches) return null
+    file = matches[1]
+    node = `${matches[2]}:${matches[3]}`
+  } else
+    [file, node] = content.trim().split('#')
 
   const response = await new Promise(resolve => {
     const headers = {
       'X-FIGMA-TOKEN': context.environment.figmaToken
-    } 
-    https.get(`https://api.figma.com/v1/images/${file}?ids=${node}`, { headers}, resolve);
+    }
+    https.get(`https://api.figma.com/v1/images/${file}?ids=${node}`, { headers }, resolve);
   })
   let data = await new Promise((resolve, reject) => {
     let data = ''
