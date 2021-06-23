adacd36dab8c46f2fc104f90cd783028f86f37fb
Jeff McAffer <jeffmcaffer@github.com>
2021-06-23 16:54:04 -0700

    allow missing file name in figma url

--- render/application/figma.js
+++ render/application/figma.js
@@ -4,7 +4,7 @@ module.exports = async ({ content, context }) => {
   let file = null
   let node = null
   if (content.match(/^https:\/\/(www.)?figma.com\/file/)) {
-    const matches = content.match(/file\/(.*?)\/.*?node-id=(.*)(?:(?:%3A)|:)(.*)$/m)
+    const matches = content.match(/file\/(.*?)[\/\?].*?node-id=(.*)(?:(?:%3A)|:)(.*)$/)
     if (!matches) return null
     file = matches[1]
     node = `${matches[2]}:${matches[3]}`
