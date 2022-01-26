1c0245bc6c7481c35f81c6587f66cbf2bf1de948
Jeff McAffer <jeffmcaffer@github.com>
2022-01-26 19:24:17 +0000

    rename http example and tweak returns/logging

--- events/http/echo/_name(...).js
+++ events/http/echo/_name(...).js
@@ -8,14 +8,14 @@ const handlers = {
   get: async (request, response, { helpers, log }) => {
     const { resource, path, params } = request
     const current = await helpers.keyValue.get(path)
-    log.info(`The current value is: ${current}`)
-    return `Hello ${params.name} from ${resource.typeName} ${resource.name}\nThe value at path: "${path}" is:\n${JSON.stringify(current)}\n`
+    log.info(`Hello ${params.name}(${query.who}) from ${resource.typeName} ${resource.name}\nThe value at path: "${path}" is:\n${JSON.stringify(current)}\n`)
+    return current
   },
   post: async (request, response, { helpers, log }) => {
-    const { path, body } = request
+    const { path, body, query } = request
     const old = await helpers.keyValue.get(path)
     await helpers.keyValue.set(path, body)
-    log.info(`Setting value for ${path} to ${JSON.stringify(body)}`)
+    log.info(`Hello ${params.name}(${query.who}) from ${resource.typeName} ${resource.name}\nSetting value for ${path} to ${JSON.stringify(body)}`)
     return old
   }
 }
